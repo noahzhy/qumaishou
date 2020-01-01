edfab545fcc5686e9db1e8d64db499e464aa2ce2
장호우 <noahzhang@hanyang.ac.kr>
2020-01-02 04:23:15 +0900

    简化爬虫

--- crawler/crawler_brand.py
+++ crawler/crawler_brand.py
@@ -12,38 +12,34 @@ domain = 'http://chn.lottedfs.cn'
 brand_name = []
 brand_url = []
 
-def requests_special_brand():
+def requests_brand(brand_name, brand_url):
+    brand = []
+    
     session = requests.Session()
-    url = 'http://chn.lottedfs.cn/kr/display/brand'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
     }
-    r = session.get(url=url, headers=headers)
-    soup = BeautifulSoup(r.text, "html5lib")
-    brandIndexList = soup.find_all('div', id='brandIndexList_GLBL')
-    brand = brandIndexList[0].select('dl > dd > ul > li > a')
-    print('Special Brand Total:', len(brand))
 
-    for i in brand:
-        brand_name.append(i.get_text().strip())
-        if ('chn.lottedfs.cn' in i['href'].strip()):
-            brand_url.append(i['href'].strip())
-        else:
-            brand_url.append(domain + i['href'].strip())
+    def requests_special_brand():
+        url = 'http://chn.lottedfs.cn/kr/display/brand'
+        r = session.get(url=url, headers=headers)
+        soup = BeautifulSoup(r.text, "html5lib")
+        brandIndexList = soup.find_all('div', id='brandIndexList_GLBL')
+        print(brandIndexList[0].select('dl > dd > ul > li > a'))
+        [brand.append(i) for i in brandIndexList[0].select('dl > dd > ul > li > a')]
+        # print('Special Brand Total:', len(brand))
 
+    def requests_brand_from_A_to_Z():
+        url = 'http://chn.lottedfs.cn/kr/display/brand/getBrandMainBrandListAjax?flag=GLBL'
+        r = session.get(url=url, headers=headers)
+        soup = BeautifulSoup(r.text, "lxml")
+        brandIndexList = soup.find_all('body')
+        [brand.append(i) for i in brandIndexList[0].select('dl > dd > ul > li > a')]
 
-def requests_brand_from_A_to_Z():
-    session = requests.Session()
-    url = 'http://chn.lottedfs.cn/kr/display/brand/getBrandMainBrandListAjax?flag=GLBL'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-    }
-    r = session.get(url=url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    brandIndexList = soup.find_all('body')
-    brand = brandIndexList[0].select('dl > dd > ul > li > a')
-    print('Brand from A to Z Total:', len(brand))
+    requests_special_brand()
+    requests_brand_from_A_to_Z()
 
+    print('Total:', len(brand))
     for i in brand:
         brand_name.append(i.get_text().strip())
         if ('chn.lottedfs.cn' in i['href'].strip()):
@@ -53,8 +49,7 @@ def requests_brand_from_A_to_Z():
 
 
 def main():
-    requests_special_brand()
-    requests_brand_from_A_to_Z()
+    requests_brand(brand_name, brand_url)
     db_tool.db_brand(brand_name, brand_url)
     pass
 
