d904398e83f571b46100de3fe438ac010d2684e2
장호우 <noahzhang@hanyang.ac.kr>
2020-01-08 00:07:51 +0900

    图片爬虫测试

--- crawler/crawler_main.py
+++ crawler/crawler_main.py
@@ -13,12 +13,10 @@ import crawler.crawler_total_product as ctp
 import crawler.crawler_proxies as proxies_api
 
 
-
 fail_counter = 0
 proxies = {}
 
-
-def main(rows):
+def total_product_info(rows):
     global fail_counter
 
     if not ctp.get_product_info(rows, proxies):
@@ -35,29 +33,37 @@ def refresh_ip():
     proxies = {'http': proxies_api.get_proxies()}
 
 
+def get_total_product_info_by_multi_processing():
+    rows = []
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    # pool = multiprocessing.Pool(1)
+
+    if os.path.exists('database/db_brand_list_check.csv'):
+        os.remove('database/db_brand_list_check.csv')
+
+    csv_file = 'database/db_brand_final.csv'
+    
+    if os.path.exists(csv_file):
+        df = pd.read_csv(csv_file)
+        # 初始化 db_brand_list_check.csv
+        result_df = pd.DataFrame(columns=['dispShopNo', 'brand_No', 'brand_name', 'total', 'status'])
+        dt.db_save('db_brand_list_check', result_df)
+
+        [rows.append(i) for i in range(0, df.shape[0])]
+
+        pool.map(total_product_info, rows)
+        pool.close()
+        pool.join()
+
+
 if __name__ == '__main__':
 
     # cb.get_all_brand('CHN')
     # cb.get_all_brand('ENG')
     # dt.intersection_db_brand()
 
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    # pool = multiprocessing.Pool(1)
-    rows = []
 
-    try: 
-        os.remove('database/db_brand_list_check.csv')
-    except:
-        pass
+    # get_total_product_info_by_multi_processing()
 
-    csv_file = 'database/db_brand_final.csv'
-    df = pd.read_csv(csv_file)
-    result_df = pd.DataFrame(columns=['dispShopNo', 'brand_No', 'brand_name', 'total', 'status'])
-    dt.db_save('db_brand_list_check', result_df)
 
-    [rows.append(i) for i in range(0, df.shape[0])]
-
-    pool.map(main, rows)
-    pool.close()
-    pool.join()
     pass
---
+++ crawler/crawler_product_img.py
@@ -0,0 +1,39 @@
+import os
+import sys
+# 导入同级目录下其他文件夹下的文件
+sys.path.append("./")
+import pandas as pd
+
+# import tools.database_tool as dt
+import crawler.crawler_brand as cb
+import crawler.crawler_product_detail as cpd
+import crawler.crawler_total_product as ctp
+import requests
+# import crawler.crawler_proxies as proxies_api
+
+def get_img(dispShopNo, img_url):
+    img_url = 'http://static.lottedfs.cn/prod/prd-img/52/90/55/00/00/02/20000559052_1.jpg'
+    html = requests.get(img_url)
+
+    img_name = img_url.split('/')[-1]
+    
+    with open(os.path.join('images', str(dispShopNo), img_name), 'wb') as file:
+        file.write(html.content)
+    pass
+
+
+def save_img_by_dispShopNo(dispShopNo, img_url):
+    if not os.path.exists('images/{}'.format(dispShopNo)):
+        os.mkdir('images/{}'.format(dispShopNo))
+    else:
+        print(dispShopNo, 'has existed')
+        get_img(dispShopNo, img_url)
+
+
+def main():
+    save_img_by_dispShopNo('test', 1)
+    pass
+
+
+if __name__ == "__main__":
+    main()
