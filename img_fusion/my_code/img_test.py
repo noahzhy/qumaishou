57b0278a0374085bc1b8e1146e72ec4f642901fd
noahzhy <noahzhy@gmail.com>
2020-01-29 20:09:44 +0800

    fix

--- img_fusion/my_code/img_test.py
+++ img_fusion/my_code/img_test.py
@@ -1,19 +1,21 @@
 __Author__ = 'Shliang'
 import numpy as np
 import cv2 as cv
+import random
+import glob
 
 
 def generate_trimap(alpha, write_name):
     
-    # img = alpha
+    img = alpha
     
-    # # 将图片转换到灰度空间
-    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # 将图片转换到灰度空间
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     
-    # # 图像平滑
-    # blur = cv.blur(gray, (3, 3))
-    # ret, thresh = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)
-    # alpha = thresh
+    # 图像平滑
+    blur = cv.blur(gray, (3, 3))
+    ret, thresh = cv.threshold(blur, 240, 255, cv.THRESH_BINARY)
+    alpha = thresh
     # alpha = cv.resize(alpha, (320,320))
     
     # cv.imshow('alpha', alpha)
@@ -28,16 +30,23 @@ def generate_trimap(alpha, write_name):
     unknown = np.array(np.less_equal(alpha, 220).astype(np.float32))
 
     unknown_dilate = cv.dilate(unknown, kernel, iterations=30)
-    trimap = (unknown_dilate - unknown) * 127 + unknown * 255
+    trimap = (unknown_dilate - unknown) * 128 + unknown * 255
     cv.imwrite("{}.png".format(write_name), trimap)
 
     return trimap.astype(np.uint8)
 
 
+def load_img():
+    random.random()
+    img_list = glob.glob('img_fusion/test/*')
+    img_path = random.choice(img_list)
+
+    return img_path
+
 if __name__ == "__main__":
     # alpha = cv.imread(r"img_fusion\test\10002212732_01.jpg")
-    img_src = r"img_fusion\test\10002336065_01.jpg"
-    img_src = r'img_fusion\test\20000758013_1.jpg'
+    img_src = load_img()
+    # img_src = r'img_fusion\test\20000758013_1.jpg'
     alpha = cv.imread(img_src)
     img = generate_trimap(alpha, img_src.split('\\')[-1].split('.')[0])
     # cv.imread(img)
