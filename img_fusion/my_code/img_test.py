11d17047862694695f828e3c7ad383501ffa68b3
noahzhy <noahzhy@gmail.com>
2020-01-31 00:41:39 +0800

    fix

--- img_fusion/my_code/img_test.py
+++ img_fusion/my_code/img_test.py
@@ -5,51 +5,6 @@ import random
 import glob
 
 
-def test2(path):
-    def get_holes(image, thresh):
-        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        im_bw = cv.threshold(gray, thresh, 255, cv.THRESH_BINARY)[1]
-        im_bw_inv = cv.bitwise_not(im_bw)
-        contour, _ = cv.findContours(im_bw_inv, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
-        
-        for cnt in contour:
-            cv.drawContours(im_bw_inv, [cnt], 0, 255, -1)
-
-        nt = cv.bitwise_not(im_bw)
-        im_bw_inv = cv.bitwise_or(im_bw_inv, nt)
-        return im_bw_inv
-
-
-    def remove_background(image, thresh, scale_factor=1, kernel_range=range(1, 3), border=None):
-        border = border or kernel_range[-1]
-        holes = get_holes(image, thresh)
-        small = cv.resize(holes, None, fx=scale_factor, fy=scale_factor)
-        bordered = cv.copyMakeBorder(small, border, border, border, border, cv.BORDER_CONSTANT)
-
-        for i in kernel_range:
-            kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2*i+1, 2*i+1))
-            bordered = cv.morphologyEx(bordered, cv.MORPH_CLOSE, kernel)
-
-        unbordered = bordered[border: -border, border: -border]
-        mask = cv.resize(unbordered, (image.shape[1], image.shape[0]))
-        fg = cv.bitwise_and(image, image, mask=mask)
-        return fg
-
-    img_path = path
-    # img_name = img_path.split('\\')[-1]
-    img = cv.imread(img_path)
-    # img = cv.resize(img, (320, 320))
-    # cv.imshow('org', img)
-    nb_img = remove_background(img, 240)
-    # nb_img = cv.resize(nb_img, (320, 320))
-    # cv.imshow('mask', nb_img)
-    # result_img = 'img_fusion/test_result/{}'.format(img_name)
-    # result_img = result_img.replace('.jpg', '.png')
-    # cv.imwrite(result_img, nb_img)
-    # cv.waitKey()
-    return nb_img
-
-
 def generate_trimap(alpha, write_name):
     
     img = alpha
@@ -59,16 +14,12 @@ def generate_trimap(alpha, write_name):
     
     # 图像平滑
     blur = cv.blur(gray, (3, 3))
-    ret, thresh = cv.threshold(blur, 0, 100, cv.THRESH_BINARY)
+    ret, thresh = cv.threshold(blur, 240, 255, cv.THRESH_BINARY)
     alpha = thresh
-
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-    unknown_dilate = cv.dilate(alpha, kernel, iterations=20)
-    trimap = (unknown_dilate - alpha) * 128 + alpha * 255
-
-    alpha = cv.resize(alpha, (720,720))
-    cv.imshow('alpha', alpha)
-    cv.waitKey()
+    # alpha = cv.resize(alpha, (320,320))
+    
+    # cv.imshow('alpha', alpha)
+    # cv.waitKey()
 
     # fg = np.array(np.less_equal(alpha, 240).astype(np.float32))
     # img = cv.resize(fg, (512, 512))
@@ -80,15 +31,15 @@ def generate_trimap(alpha, write_name):
 
     unknown_dilate = cv.dilate(unknown, kernel, iterations=30)
     trimap = (unknown_dilate - unknown) * 128 + unknown * 255
-    cv.imwrite(r"E:\github_clone\indexnet_matting\examples\images\test_origin.png", img)
-    cv.imwrite(r"E:\github_clone\indexnet_matting\examples\trimaps\test_trimap.png", trimap)
+    cv.imwrite(r"img_fusion\image_matting\data\input\test.png", img)
+    cv.imwrite(r"img_fusion\image_matting\data\trimap\test.png", trimap)
 
     return trimap.astype(np.uint8)
 
 
 def load_img():
     random.random()
-    img_list = glob.glob('img_fusion/test/*')
+    img_list = glob.glob(r'img_fusion\test\*')
     img_path = random.choice(img_list)
 
     return img_path
@@ -97,8 +48,7 @@ if __name__ == "__main__":
     # alpha = cv.imread(r"img_fusion\test\10002212732_01.jpg")
     img_src = load_img()
     # # img_src = r'img_fusion\test\20000758013_1.jpg'
-    # alpha = cv.imread(img_src)
-    alpha = test2(img_src)
+    alpha = cv.imread(img_src)
     img = generate_trimap(alpha, img_src.split('\\')[-1].split('.')[0])
     img = cv.resize(img, (512, 512))
     cv.imshow("trimp", img)
--- test.py
+++ test.py
@@ -1,25 +1,34 @@
-# k = [1]
-# count = 0
-# flag = False
-# for i in k:
-#     count += 1
-#     print(count)
-#     if not flag:
-#         k.append(2)
-#         k.append(3)
-#         k.append(4)
-#         flag = True
-
-# [print(i) for i in range(2, int(5))]
-
-def test(flag=1):
-    if (flag==1):
-        return 1
-    else:
-        return False
+import numpy as np
+from PIL import Image as image
+from sklearn.cluster import KMeans
 
 
-if (test(flag=0)):
-    print(test(flag=1))
-else:
-    print('555')
+def loadData(filePath):
+    f = open(filePath, 'rb') #以二进制形式打开文件
+    data = []
+    img = image.open(f)
+    m,n = img.size
+    for i in range(m):
+        for j in range(n):
+            #将每个像素点RGB颜色处理到0-1范围内
+            x,y,z =img.getpixel((i,j))
+            #将颜色值存入data内
+            data.append([x/256.0, y/256.0, z/256.0])
+    f.close()
+    #以矩阵的形式返回data，以及图片大小
+    return np.mat(data), m, n
+
+imgData, row, col = loadData(r'img_fusion\test\20000543988_1.jpg')#加载数据
+
+km = KMeans(n_clusters=2)
+#聚类获得每个像素所属的类别
+label = km.fit_predict(imgData)
+label = label.reshape([row, col])
+#创建一张新的灰度图以保存聚类后的结果
+pic_new = image.new("L", (row, col))
+#根据类别向图片中添加灰度值
+for i in range(row):
+    for j in range(col):
+        pic_new.putpixel((i, j), int(256/(label[i][j]+1)))
+#以JPEG格式保存图像
+pic_new.save("test.png", "PNG")
